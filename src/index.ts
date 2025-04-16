e980959f2e58c2fe3df2c5cd5f8fe6e949275681
AndroPyro28 <menandroeugenio1028@gmail.com>
2025-04-16 16:11:30 +0800

    save

--- src/index.ts
+++ src/index.ts
@@ -4,31 +4,36 @@ import { proxy } from 'hono/proxy'
 import { cors } from 'hono/cors'
 import { secureHeaders } from 'hono/secure-headers'
 import sampleControllers from './routes/user/user.controller'
+import { authMiddleware, TAuthVariables } from './middlewares/auth.middleware'
 
-const app = new Hono().basePath('/api')
+const app = new Hono<{ Variables: TAuthVariables }>().basePath('/api')
 .use('/api/*', cors())
 .use(secureHeaders())
 .use(logger())
-.route('/users', sampleControllers)
+// .use("*", authMiddleware)
+
 // helper to get dynamic path
+
 const stripPrefix = (prefix: string, url: string) =>
   url.replace(new RegExp(/^\v1/), '/api')
  
 const createProxyRoute = (prefix: string, targetBase: string) => {
 
   app.all(prefix + '/*', async (c) => {
+    
     const path = stripPrefix(prefix, c.req.url)
 
-    const targetUrl = `${targetBase}/api`
-    console.log("URL", targetUrl)
-    return proxy(targetUrl, {
-      method: c.req.method,
-    //   body: await c.req.json(),
+    const targetUrl = `${targetBase}/api${path.split("v1")[1]}`
+    console.log("PROXY REQUEST URL", targetUrl)
+    return await proxy(targetUrl, {
+      body: JSON.stringify(await c.req.json()),
+      ...c.req,
       headers: {
         ...c.req.header(),
         'X-Forwarded-For': c.req.header('x-forwarded-for') ?? '127.0.0.1',
         'X-Forwarded-Host': c.req.header('host'),
         Authorization: undefined,
+        "Content-Type": "application/json"
       }
     })
   })
--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,11 +1,61 @@
 
 import { createMiddleware } from "hono/factory";
+import { verifyIdToken } from "../libs/google-auth";
+
+export type userAuthPayload = {
+  userId: string;
+  email: string | undefined;
+  name: string | undefined;
+}
+
 export const authMiddleware = createMiddleware(async (c, next) => {
-  const user = c.get("authUser");
- 
-  await next();
+
+  const authHeader = c.req.header('authorization')
+
+  // Bearer ${token}
+  // get the token only
+
+  const token = authHeader && authHeader.split(" ")[1];
+
+  if(!token) {
+    return c.json({
+      error: "Access denied! No Token provided"
+    }, 400)
+  }
+
+  try {
+
+    const payload = await verifyIdToken(token)
+
+    if(!payload) {
+      throw new Error("Access denied! Invalid Token")
+    }
+
+    const user = {
+      userId: payload['sub'],
+      name: payload['name'],
+      email: payload['email']
+    }
+
+    // * ADD user object in the context
+    c.set("user", user)
+
+    // * ADD userId in the headers
+    c.header('x-user-id', payload['sub'])
+
+    // * ADD email and name in the headers (optional)
+    c.header('x-user-email', payload['email'])
+    c.header('x-user-name', payload['name'])
+    await next();
+
+  } catch (error) {
+    console.error("AUTH TOKEN VERIFICATION ERROR: ", error)
+    return c.json({
+      error: "Access denied! Invalid Token"
+    }, 401)
+  }
 });
 
 export type TAuthVariables = {
-  sample:string
+  user: userAuthPayload
 };
